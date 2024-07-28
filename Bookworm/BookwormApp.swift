ca6bc52393b65a9e841dd278dbad814a607c3383
fernandocallejasr <ferd.callejas@gmail.com>
2024-07-27 23:55:20 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Fernando Callejas on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
